21cd7d6d86cf763c319d18b757100f555b5e6447
rhdzmota <rhdzmota@mxquants.com>
2017-11-07 12:07:38 -0600

    test

--- app/mod_stocks/controller/__init__.py
+++ app/mod_stocks/controller/__init__.py
@@ -9,6 +9,16 @@ mod_stocks = Blueprint('stocks', __name__, url_prefix='/stocks')
 def get_index():
     return "BMV Stocks"
 
+@mod_stocks.route('/test', methods=['GET', 'POST'])
+def test():
+    from selenium import webdriver
+
+    browser = webdriver.PhantomJS()
+    browser.get("http://www.google.com")  # This does not throw an exception if it got a 404
+
+    html = browser.page_source
+    return str(html)
+
 
 @mod_stocks.route('/statistics/<stock>', methods=['GET', 'POST'])
 def market_rates(stock):
